7345049c0dc3d73daa71338fc303076aa8efa8ba
Narayan Gautam <46322720+gnarayyan@users.noreply.github.com>
2025-05-20 23:55:15 +0545

    Add route registration functionality to EasyAPI class

--- easyapi/main.py
+++ easyapi/main.py
@@ -1,6 +1,15 @@
+from easyapi.utils.exceptions import RouteAlreadyExistsError
+
+
 class EasyAPI:
+    def __init__(self):
+        self.routes = dict()
+    
     async def __call__(self, scope, receive, send):
         assert scope['type'] == 'http'
+        
+        print(f'Method: {scope["method"]}')
+        print(f'Path: {scope["path"]}')
 
         body = b'Hello, world! from class based implementation'
         content_length = str(len(body)).encode()
@@ -18,3 +27,16 @@ class EasyAPI:
             'body': body,
         })
 
+    def get(self, path=None):
+        def wrapper(handler):
+            path_name = path or ''
+            if path_name not in self.routes:
+                self.routes[path_name] = {}
+            
+            if 'GET' not in self.routes[path_name]:
+                self.routes[path_name]['GET'] = handler
+                print(f'GET  {path_name} Registered')
+            else:
+                raise RouteAlreadyExistsError(f'GET {path_name} already registered')
+        return wrapper
+        
