2f08abb1a622f6db8ec3a42625351d425d78e3d7
Narayan Gautam <46322720+gnarayyan@users.noreply.github.com>
2025-05-20 22:46:31 +0545

    Implement basic HTTP server using Uvicorn with a simple "Hello, world!" response

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello")
+import uvicorn
+
+async def app(scope, receive, send):
+    assert scope['type'] == 'http'
+
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [
+            (b'content-type', b'text/plain'),
+            (b'content-length', b'13'),
+        ],
+    })
+    await send({
+        'type': 'http.response.body',
+        'body': b'Hello, world!',
+    })
+    
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info")
