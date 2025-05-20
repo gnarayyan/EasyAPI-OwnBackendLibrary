1f110f8d51f21eb95d99a9fba6370853f457424e
Narayan Gautam <46322720+gnarayyan@users.noreply.github.com>
2025-05-20 22:52:44 +0545

    Refactor HTTP server implementation to use a class-based approach and enhance response message

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import uvicorn
 
-async def app(scope, receive, send):
-    assert scope['type'] == 'http'
+class App:
+    async def __call__(self, scope, receive, send):
+        assert scope['type'] == 'http'
 
-    await send({
-        'type': 'http.response.start',
-        'status': 200,
-        'headers': [
-            (b'content-type', b'text/plain'),
-            (b'content-length', b'13'),
-        ],
-    })
-    await send({
-        'type': 'http.response.body',
-        'body': b'Hello, world!',
-    })
-    
+        body = b'Hello, world! from class based implementation'
+        content_length = str(len(body)).encode()
+
+        await send({
+            'type': 'http.response.start',
+            'status': 200,
+            'headers': [
+                (b'content-type', b'text/plain'),
+                (b'content-length', content_length),
+            ],
+        })
+        await send({
+            'type': 'http.response.body',
+            'body': body,
+        })
+
+app = App()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=5000, log_level="info")
+    uvicorn.run("main:app", port=5000, log_level="info", reload=True)
