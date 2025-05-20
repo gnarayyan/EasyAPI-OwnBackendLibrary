634d5af272583f1371c398d8c337a69428524112
Narayan Gautam <46322720+gnarayyan@users.noreply.github.com>
2025-05-20 23:56:57 +0545

    Registered 2 seperate get routes

--- example/main.py
+++ example/main.py
@@ -2,3 +2,11 @@ from easyapi.main import EasyAPI
 
 
 app = EasyAPI()
+
+@app.get('/users')
+def get_users(req, res):
+  res.send(['rivaan', 'naman'])
+
+@app.get('/user')
+def get_users(req, res):
+  res.send('rivaan')
