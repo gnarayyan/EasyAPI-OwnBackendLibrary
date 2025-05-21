8fad472de3906e703376e685513ad32efd8b9418
Narayan Gautam <46322720+gnarayyan@users.noreply.github.com>
2025-05-21 18:44:02 +0545

    Enable parsing request body

--- easyapi/utils/request.py
+++ easyapi/utils/request.py
@@ -1,18 +1,33 @@
+import json
+
+
 class Request:
     """Represents the incoming HTTP request."""
 
     def __init__(self, scope, receive):
-        self.scope = scope # Will be used later for parsing query string
-        self.receive = receive
+        self._scope = scope  # Will be used later for parsing query string
+        self._receive = receive
+        self.body = None
 
-    async def get_body(self):
+    async def initialize(self):
+        """Asynchronous initialization of the request, reads the body."""
+        self.body = await self._parse_body()
+
+    async def _parse_body(self):
         """Read and return the entire body from an incoming ASGI message."""
         body = b''
         more_body = True
 
         while more_body:
-            message = await self.receive()
+            message = await self._receive()
             body += message.get('body', b'')
             more_body = message.get('more_body', False)
 
-        return body
+        # Decode the byte string to a normal string
+        body_str = body.decode('utf-8')
+
+        # Parse the string as JSON (convert to a Python dictionary)
+        try:
+            return json.loads(body_str)
+        except json.JSONDecodeError:
+            return None  # If JSON decoding fails, set it to None
