4ebf2ade192c2bde2985eafd70f1647a8841ee67
Narayan Gautam <46322720+gnarayyan@users.noreply.github.com>
2025-05-21 17:44:44 +0545

    Refactor route handlers to use async functions and improve response handling with Request and Response classes

--- example/main.py
+++ example/main.py
@@ -1,12 +1,21 @@
 from easyapi.main import EasyAPI
+from easyapi.utils.request import Request
+from easyapi.utils.response import Response
 
 
 app = EasyAPI()
 
 @app.get('/users')
-def get_users(req, res):
-  res.send(['rivaan', 'naman'])
+async def get_users(req:Request, res:Response):
+  await res.json(['rivaan', 'naman'])
 
 @app.get('/user')
-def get_users(req, res):
-  res.send('rivaan')
+async def get_users(req:Request, res:Response):
+  
+  await res.send( status=400, message={"message": "Success","name" :'rivaan'}, content_type='application/json')
+
+@app.get("/hello")
+async def hello_handler(req:Request, res:Response):
+    
+    # name = params.get("name", ["world"])[0]
+    await res.text(f"Hello, Narayan!") 
