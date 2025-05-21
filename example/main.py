02c38c072b1746e01c881f7743a75f1cbe0ec2b9
Narayan Gautam <46322720+gnarayyan@users.noreply.github.com>
2025-05-21 18:21:49 +0545

    Add post request example to view status code in response

--- example/main.py
+++ example/main.py
@@ -5,17 +5,28 @@ from easyapi.utils.response import Response
 
 app = EasyAPI()
 
+
 @app.get('/users')
-async def get_users(req:Request, res:Response):
-  await res.json(['rivaan', 'naman'])
+async def get_users(req: Request, res: Response):
+    await res.json(['rivaan', 'naman'])
+
 
 @app.get('/user')
-async def get_users(req:Request, res:Response):
-  
-  await res.send( status=400, message={"message": "Success","name" :'rivaan'}, content_type='application/json')
+async def get_users(req: Request, res: Response):
+    await res.send(
+        status=400,
+        message={"message": "Success", "name": 'rivaan'},
+        content_type='application/json',
+    )
+
+
+@app.post('/user')
+async def get_users(req: Request, res: Response):
+    await res.status(201).json({"success": True, "message": 'Rivaan Added'})
+
 
 @app.get("/hello")
-async def hello_handler(req:Request, res:Response):
-    
+async def hello_handler(req: Request, res: Response):
+
     # name = params.get("name", ["world"])[0]
-    await res.text(f"Hello, Narayan!") 
+    await res.text(f"Hello, Narayan!")
